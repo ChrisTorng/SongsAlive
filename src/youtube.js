6d190286392f2dab4066e96ade7c452e68608961
ChrisTorng <ChrisTorng@hotmail.com>
2021-08-22 11:24:50 +0800

    youtube.js add all event, cue onReady, enable cued

--- src/youtube.js
+++ src/youtube.js
@@ -11,24 +11,75 @@ function loadPlayer(newSong) {
 var player;
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
-        videoId: song.videoId,
         origin: window.location.origin,
         playerVars: {
             'playsinline': 1
         },
         events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
+            'onReady': onReady,
+            'onStateChange': onStateChange,
+            'onPlaybackQualityChange': onPlaybackQualityChange,
+            'onPlaybackRateChange': onPlaybackRateChange,
+            'onError': onError,
+            'onApiChange': onApiChange
         }
     });
 }
 
-function onPlayerReady(event) {
-    var control = document.getElementById('control');
-    control.classList.remove('disabled');
+function onReady(event) {
+    console.log("onReady", event);
+    event.target.cueVideoById(song.videoId);
 }
 
-function onPlayerStateChange(event) {
+function onStateChange(event) {
+    console.log("onStateChange", playerState(event.data));
+    if (event.data === YT.PlayerState.CUED) {
+        var control = document.getElementById('control');
+        control.classList.remove('disabled');
+    }
+}
+
+function playerState(state) {
+    switch (state) {
+        case YT.PlayerState.UNSTARTED:
+            return "unstarted";
+        case YT.PlayerState.ENDED:
+            return "ended";
+        case YT.PlayerState.PLAYING:
+            return "playing";
+        case YT.PlayerState.PAUSED:
+            return "paused";
+        case YT.PlayerState.BUFFERING:
+            return "buffering";
+        case YT.PlayerState.CUED:
+            return "cued";
+        default:
+            return state;
+    }
+}
+
+function onPlaybackQualityChange(event) {
+    console.log("onPlaybackQualityChange", event.data);
+}
+
+function onPlaybackRateChange(event) {
+    console.log("onPlaybackRateChange", event.data);
+}
+
+function onError(event) {
+    console.log("onError", event.data);
+}
+
+function onApiChange(event) {
+    console.log('onApiChange');
+    console.log(player.getOptions());
+
+    var options = player.getOptions('captions');
+    console.log(options);
+
+    for (var index in options) {
+        console.log(options[index], player.getOption('captions', options[index]));
+    }
 }
 
 function onPlayButton() {
