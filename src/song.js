10e1b758fb6ced91d8b8ec33ff8c9d4dbad67c7b
ChrisTorng <ChrisTorng@hotmail.com>
2021-08-28 21:49:36 +0800

    song.js add setTitle and setSections

--- src/song.js
+++ src/song.js
@@ -11,11 +11,30 @@ fetch('songs/' + songId + '.json')
 
 function processSong(song) {
     loadPlayer(song);
+    setTitle(song);
+    setSections(song);
+}
 
+function setTitle(song) {
     document.title = song.title + ' - ' + document.title;
-
     const url = 'https://www.youtube.com/watch?v=' + song.videoId;
     const titleAnchor = document.getElementById('title');
     titleAnchor.href = url;
     titleAnchor.innerText = song.title;
+}
+
+function setSections(song) {
+    var sectionsHtml = '';
+    for (var index in song.sections) {
+        sectionsHtml += '<li onclick="selectSection(' + index + ')" ' +
+            'title="' + song.sections[index].detail + '">' +
+            '<div>' + song.sections[index].title + '</div>' +
+            '<div>' + song.sections[index].detail.substring(0, 4) + '</div>' +
+            '<div>' + song.sections[index].start + '<br/>' +
+            song.sections[index].end + '</div>' +
+            '</li>\n';
+    }
+
+    const sections = document.getElementById('sections');
+    sections.innerHTML = sectionsHtml;
 }
